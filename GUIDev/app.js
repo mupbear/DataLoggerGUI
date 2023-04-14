43c34f0f49fc09929afd7212ecf8cab1b3c90f38
mupbear <dana.school.aa@gmail.com>
2023-04-14 13:07:01 +0200

    3 Differrent graphs with fake data, just figured out Chart.js already has dataset selection functionality

--- GUIDev/app.js
+++ GUIDev/app.js
@@ -1,5 +1,84 @@
+var ctx1 = document.getElementById('myChart1').getContext('2d');
+		var myChart1 = new Chart(ctx1, {
+			type: 'line',
+			options: chartOptions
+		});
 
+var ctx2 = document.getElementById('myChart2').getContext('2d');
+		var myChart2 = new Chart(ctx2, {
+			type: 'line',
+			options: chartOptions
+		});
 
+        const chartData = {
+            labels: [""], // You can set custom labels here
+            datasets: [
+              {
+                label: "None Selected",
+                data: [],
+                backgroundColor: "rgba(0,0,0,0)",
+                borderColor: "rgba(0,0,0,0)",
+                borderWidth: 1
+              },
+              {
+                label: "Car Speed",
+                data: [
+                  { x: "2022-01-01T01:00:00Z", y: 60 },
+                  { x: "2022-01-01T02:00:00Z", y: 55 },
+                  { x: "2022-01-01T03:00:00Z", y: 65 }
+                ],
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgba(255, 99, 132, 1)",
+                borderWidth: 1
+              },
+              {
+                label: "Bike Speed",
+                data: [
+                  { x: "2022-01-01T01:00:00Z", y: 15 },
+                  { x: "2022-01-01T02:00:00Z", y: 20 },
+                  { x: "2022-01-01T03:00:00Z", y: 25 }
+                ],
+                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                borderColor: "rgba(54, 162, 235, 1)",
+                borderWidth: 1
+              }
+            ]
+          };
+          
+
+ var ctx3 = document.getElementById('myChart3').getContext('2d');
+		var myChart3 = new Chart(ctx3, {
+			type: 'line',
+            data: chartData,
+			options: {
+                scales: {
+                  xAxes: [
+                    {
+                      type: "time",
+                      time: {
+                        parser: "YYYY-MM-DDTHH:mm:ssZ",
+                        unit: "hour",
+                        displayFormats: {
+                          hour: "MMM D, hA"
+                        }
+                      },
+                      ticks: {
+                        autoSkip: true,
+                        maxTicksLimit: 10
+                      }
+                    }
+                  ],
+                  yAxes: [
+                    {
+                      scaleLabel: {
+                        display: true,
+                        labelString: "Speed (km/h)"
+                      }
+                    }
+                  ]
+                }
+              }
+		});
 
 
 var chartOptions = {
@@ -21,23 +100,6 @@ var chartOptions = {
     }
 };
 
-var ctx1 = document.getElementById('myChart1').getContext('2d');
-		var myChart1 = new Chart(ctx1, {
-			type: 'line',
-			options: chartOptions
-		});
-
-var ctx2 = document.getElementById('myChart2').getContext('2d');
-		var myChart2 = new Chart(ctx2, {
-			type: 'line',
-			options: chartOptions
-		});
-
- var ctx3 = document.getElementById('myChart3').getContext('2d');
-		var myChart3 = new Chart(ctx3, {
-			type: 'line',
-			options: chartOptions
-		});
 
 
 function updateChart(chartName, selectID) {
@@ -60,21 +122,43 @@ function updateChart(chartName, selectID) {
 $.getJSON('data/ID-data.json', function(data) {
     // Loop through each option and add it to the dropdown menu
     $.each(data, function(index, option) {
-        $('#select-chart1').append($('<option>', {
-            value: option.value,
-            text: option.label
-        }));
-    });
-    $.each(data, function(index, option) {
         $('#select-chart2').append($('<option>', {
             value: option.value,
             text: option.label
         }));
     });
+
     $.each(data, function(index, option) {
-        $('#select-chart3').append($('<option>', {
+        $('#select-unit').append($('<option>', {
             value: option.value,
             text: option.label
         }));
     });
 });
+
+
+    function selectDataSet(selectID) {
+        var selectBox = document.getElementById(selectID);
+        var selectedValue = selectBox.options[selectBox.selectedIndex].value;
+    
+        $.getJSON('data/ID-data.json', function(data) {
+            var selectedData = data.find(function(item) {
+                return item.value === selectedValue;
+            });
+    
+            if (selectedData) {
+                $('#select-dataset').empty(); // clear the current options
+                $.each(selectedData.datasets, function(index, option) {
+                    $('#select-dataset').append($('<option>', {
+                        value: option.value,
+                        text: option.label
+                    }));
+                });
+            }
+        });
+    }
+    
+    
+
+
+      
--- GUIDev/data.php
+++ GUIDev/data.php
@@ -12,23 +12,20 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 	<title>Data</title>
 	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-	<link rel="stylesheet" type="text/css" href="dark-mode.css">
+	<!--<link rel="stylesheet" type="text/css" href="dark-mode.css">-->
 
 </head>
 <body>
 
 <br>
 	<!-- First chart -->
-	<label for="select-chart1">Select Chart 1:</label> 
-	<select id="select-chart1" onchange="updateChart(myChart1, 'select-chart1')"></select> <button onclick="addDropdown()">Add Dataset</button><label for="select1">Select Dataset:</label>
-    		<select id="select1">
-	<div id="dropdowns">
-  		<div class="dropdown">
-    		<label for="select1">Select Dataset:</label>
-    		<select id="select1">
-    		</select>
-  		</div>
-	</div>
+	<p>Chart 1</p>
+	<label for="select-unit">Select Unit:</label>
+	<select id="select-unit" onchange="selectDataSet('select-unit')"></select>
+
+	<label for="select-dataset">Select Dataset:</label>
+	<select id="select-dataset" onchange="updateData(myChart1, 'select-unit', 'select-dataset')" ></select>
+	
 	<canvas id="myChart1" style="height: 50px; width: 400px;"></canvas>
 
 	<!-- Second chart -->
@@ -38,11 +35,14 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 
 	<!-- Third chart -->
 	<label for="select-chart3">Select Chart 3:</label>
-	<select id="select-chart3" onchange="updateChart(myChart3, 'select-chart3')"></select>
+	<select id="select-chart3" ></select>
+	<button id="myButton" onclick="showData()"></button>
 	<canvas id="myChart3" style="height: 50px; width: 400px;"></canvas>
 
 	<!-- The script for updating the charts and checking json file. -->
 	<script src="app.js"></script>
+	<script>
+	</script>
 </body>
 </html>
 
