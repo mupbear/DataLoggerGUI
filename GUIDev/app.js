a98fd2e58088e7dc6a79a66721b80953c36ca706
mupbear <dana.school.aa@gmail.com>
2023-04-24 16:59:45 +0200

    Changed the graph layout to have multiple axes in 1 graph, all with the same x axis

--- GUIDev/app.js
+++ GUIDev/app.js
@@ -1,118 +1,84 @@
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'line',
-  data: {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My Dataset',
-      data: [1, 2, 3, 4, 5, 6, 7],
-      borderColor: 'rgb(255, 99, 132)',
-      borderWidth: 2
-    }]
-  },
-  options: {
-    maintainAspectRatio: false,
-    // Add any additional chart options here
-  }
-});
-const graphBody = document.querySelector('.containerBody');
-
-if(myChart.data.datasets[0].data.length > 3){
-  graphBody.style.width = '5000px';
-}
-
-
 var dark = false;
 
-var ctx1 = document.getElementById('myChart1').getContext('2d');
-		var myChart1 = new Chart(ctx1, {
-			type: 'line',
-			options: chartOptions
-		});
-
-var ctx2 = document.getElementById('myChart2').getContext('2d');
-		var myChart2 = new Chart(ctx2, {
-			type: 'line',
-			options: chartOptions
-    });
-
- var ctx3 = document.getElementById('myChart3').getContext('2d');
-		var myChart3 = new Chart(ctx3, {
-			type: 'line',
-			options: chartOptions
-		});
-    
- var ctx4 = document.getElementById('myChart4').getContext('2d');
-		var myChart4 = new Chart(ctx4, {
-			type: 'line',
-			options: chartOptions
-		});
-
- var ctx5 = document.getElementById('myChart5').getContext('2d');
-		var myChart5 = new Chart(ctx5, {
-			type: 'line',
-			options: chartOptions
-		});
+const data = {
+  datasets: [
+    {
+      label: 'Sales',
+      data: [{"x": "2022-01-01T01:00:00Z", "y": 30},
+      {"x": "2022-01-01T02:00:00Z", "y": 45},
+      {"x": "2022-01-01T03:00:00Z", "y": 55},
+      {"x": "2022-01-01T04:00:00Z", "y": 50},
+      {"x": "2022-01-01T05:00:00Z", "y": 60},
+      {"x": "2022-01-01T06:00:00Z", "y": 65},
+      {"x": "2022-01-01T07:00:00Z", "y": 70},
+      {"x": "2022-01-01T08:00:00Z", "y": 60},
+      {"x": "2022-01-01T09:00:00Z", "y": 50},
+      {"x": "2022-01-01T10:00:00Z", "y": 55}],
+      borderWidth: 1,
+      yAxisID: 'y1'
+    },
+    {
+      label: 'Expenses',
+      data: [{"x": "2022-01-01T01:00:00Z", "y": 20},
+      {"x": "2022-01-01T02:00:00Z", "y": 35},
+      {"x": "2022-01-01T03:00:00Z", "y": 45},
+      {"x": "2022-01-01T04:00:00Z", "y": 40},
+      {"x": "2022-01-01T05:00:00Z", "y": 30},
+      {"x": "2022-01-01T06:00:00Z", "y": 40},
+      {"x": "2022-01-01T07:00:00Z", "y": 50},
+      {"x": "2022-01-01T08:00:00Z", "y": 60},
+      {"x": "2022-01-01T09:00:00Z", "y": 40},
+      {"x": "2022-01-01T10:00:00Z", "y": 30}],
+      borderWidth: 1,
+      yAxisID: 'y1'
+    },
+    {
+      label: 'Profit',
+      data: [{"x": "2022-01-01T01:00:00Z", "y": 20},
+      {"x": "2022-01-01T02:00:00Z", "y": 35},
+      {"x": "2022-01-01T03:00:00Z", "y": 45},
+      {"x": "2022-01-01T04:30:00Z", "y": 40},
+      {"x": "2022-01-01T05:30:00Z", "y": 30},
+      {"x": "2022-01-01T06:30:00Z", "y": 40},
+      {"x": "2022-01-01T07:30:00Z", "y": 50},
+      {"x": "2022-01-01T08:30:00Z", "y": 60},
+      {"x": "2022-01-01T09:00:00Z", "y": 40},
+      {"x": "2022-01-01T10:00:00Z", "y": 30}],
+      borderWidth: 1,
+      yAxisID: 'y2'
+    },
+  ]
+};
 
 var chartOptionsDark = {
-      maintainAspectRatio: false,
-      scales: {
-        x: {
-          ticks: {
-            color: '#81c8bd'
-          },
-          grid: {
-            color: '#81c8bd'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#81c8bd'
-          },
-          grid: {
-            color: '#81c8bd'
-          }
-        }
-      },
-        //maintainAspectRatio: false, // disable aspect ratio
-        plugins: {
-          legend:{
-            labels: {
-              color: 'white'
-            },
-          },
-          zoom: {
-              pan: {
-                    enabled: true,
-                    mode: 'x',
-                    threshold: 10,
-              },
-           }
-        },
-      
-    };
-
-
-var chartOptions = {
-   maintainAspectRatio: false,
-  scales: {
-    x: {
-      ticks: {
-        color: 'white'
-      },
-      grid: {
-        color: 'white'
-      }
-    },
-    y: {
-      ticks: {
-        color: 'white'
-      },
-      grid: {
-        color: 'white'
-      }
-    }
+  pointRadius: 0,
+  interaction: {
+    intersect: false,
+    mode: 'nearest',
+},
+scales: {
+  x: {
+    ticks: {color: '#81C8BD'},
+    grid: {color: '#81C8BD'},
+    position: 'top',
+    type: 'time',
+time: {
+unit: 'hour',
+displayFormats: {
+  hour:'HH:mm:ss.SSS',
+},}
   },
+  y1: {
+    ticks: {color: '#81C8BD'},
+    grid: {color: '#81C8BD'},
+      stack: 'demo'
+  },
+  y2: {
+    ticks: {color: '#81C8BD'},
+    grid: {color: '#81C8BD'},
+      stack: 'demo'
+  },
+},
     //maintainAspectRatio: false, // disable aspect ratio
     plugins: {
       legend:{
@@ -120,28 +86,58 @@ var chartOptions = {
           color: 'white'
         },
       },
-      zoom: {
-          pan: {
-                enabled: true,
-                mode: 'x',
-                threshold: 10,
-          },
-       }
     },
   
-  };
+};
 
+var chartOptions = {
+  pointRadius: 0,
+  interaction: {
+    intersect: false,
+    mode: 'nearest',
+},
+scales: {
+  x: {
+    ticks: {color: 'white'},
+    grid: {color: 'white'},
+    position: 'top',
+    type: 'time',
+time: {
+unit: 'hour',
+displayFormats: {
+  hour:'HH:mm:ss.SSS',
+},}
+  },
+  y1: {
+    ticks: {color: 'white'},
+    grid: {color: 'white'},
+      stack: 'demo'
+  },
+  y2: {
+    ticks: {color: 'white'},
+    grid: {color: 'white'},
+      stack: 'demo'
+  },
+},
+    //maintainAspectRatio: false, // disable aspect ratio
+    plugins: {
+      legend:{
+        labels: {
+          color: 'white'
+        },
+      },
+    },
+  
+};
 
-function resetZoomChart(chartName){
-  chartName.resetZoom();
-}
+var ctx = document.getElementById('myChart').getContext('2d');
+		var myChart = new Chart(ctx, {
+      type: 'line',
+      data: data,
+      options: chartOptions
 
-function zoomInButton(chartName){
-  chartName.zoom(1.1);
-}
-function zoomOutButton(chartName){
-  chartName.zoom(0.9);
-}
+    });
+
 
 function updateGraph(chartName, selectID) {
     var selectBox = document.getElementById(selectID);
@@ -163,37 +159,13 @@ function updateGraph(chartName, selectID) {
             chartName.update();
         }
     })
-}
+};
 
 // Load JSON file
 $.getJSON('data/fake-data.json', function(data) {
     // Loop through each option and add it to the dropdown menu
     $.each(data, function(index, option) {
-        $('#select-chart1').append($('<option>', {
-            value: option.value,
-            text: option.label
-        }));
-    });
-    $.each(data, function(index, option) {
-        $('#select-chart2').append($('<option>', {
-            value: option.value,
-            text: option.label
-        }));
-    });
-    $.each(data, function(index, option) {
-        $('#select-chart3').append($('<option>', {
-            value: option.value,
-            text: option.label
-        }));
-    }); 
-    $.each(data, function(index, option) {
-        $('#select-chart4').append($('<option>', {
-            value: option.value,
-            text: option.label
-        }));
-    }); 
-    $.each(data, function(index, option) {
-        $('#select-chart5').append($('<option>', {
+        $('#select-chart').append($('<option>', {
             value: option.value,
             text: option.label
         }));
@@ -207,42 +179,18 @@ const body = document.body;
 checkbox.addEventListener('change', function() {
   if (this.checked) {
     body.classList.add('dark-mode');
-    chart1Div.classList.add('chartBox-dark');
-    chart2Div.classList.add('chartBox-dark');
-    chart3Div.classList.add('chartBox-dark');
-    chart4Div.classList.add('chartBox-dark');
-    chart5Div.classList.add('chartBox-dark');
+    chartDiv.classList.add('chartBox-dark');
 
     dark = true;
-    myChart1.options = chartOptionsDark;
-    myChart2.options = chartOptionsDark;
-    myChart3.options = chartOptionsDark;
-    myChart4.options = chartOptionsDark;
-    myChart5.options = chartOptionsDark;
-    myChart1.update();
-    myChart2.update();
-    myChart3.update();
-    myChart4.update();
-    myChart5.update();
+    myChart.options = chartOptionsDark;
+    myChart.update();
   } else {
     body.classList.remove('dark-mode');
-    chart1Div.classList.remove('chartBox-dark');
-    chart2Div.classList.remove('chartBox-dark');
-    chart3Div.classList.remove('chartBox-dark');
-    chart4Div.classList.remove('chartBox-dark');
-    chart5Div.classList.remove('chartBox-dark');
+    chartDiv.classList.remove('chartBox-dark');
 
     dark = false;
-    myChart1.options = chartOptions;
-    myChart2.options = chartOptions;
-    myChart3.options = chartOptions;
-    myChart4.options = chartOptions;
-    myChart5.options = chartOptions;
-    myChart1.update();
-    myChart2.update();
-    myChart3.update();
-    myChart4.update();
-    myChart5.update();
+    myChart.options = chartOptions;
+    myChart.update();
   }
 });
 
--- GUIDev/data.php
+++ GUIDev/data.php
@@ -16,6 +16,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js" integrity="sha512-UXumZrZNiOwnTcZSHLOfcTs0aos2MzBWHXOHOuB0J/R44QB0dwY5JgfbvljXcklVf65Gc4El6RjZ+lnwd2az2g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/chartjs-plugin-zoom/2.0.1/chartjs-plugin-zoom.min.js" integrity="sha512-wUYbRPLV5zs6IqvWd88HIqZU/b8TBx+I8LEioQ/UC0t5EMCLApqhIAnUg7EsAzdbhhdgW07TqYDdH3QEXRcPOQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 	<script src="https://unpkg.com/chart.js-plugin-labels-dv/dist/chartjs-plugin-labels.min.js"></script>
+	<script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
+
 
 	<link rel="stylesheet" href="css/style.css">
 	<link rel="stylesheet" href="css/dark-mode.css" media="(prefers-color-scheme: dark)">
@@ -29,62 +31,15 @@ if (isset($_SESSION['id']) && isset($_SESSION['username'])) {
 	<form method="POST" action="home.php"><button> Home </button></form> 
 	<form method="POST" action="admin/admin.php"><button> Admin </button></form>
 	<form method="POST" action="gps.php"><button> Map </button></form>
-  
-	<!-- Test Chart -->
-		<div class="container">
-			<div class="containerBody">
-  			<canvas id="myChart"></canvas>
-			</div>
-		</div>
 
 
-	<!-- First chart -->
-	<div class="container">
-		<div id= "chart1Div" class="chartBox">
-			<div class="containerBody">
-	<label for="select-chart1">Select Data:</label> 
-	<select id="select-chart1" onchange="updateGraph(myChart1, 'select-chart1')"></select><button onclick="resetZoomChart(myChart1)">Reset Zoom</button><button onclick="zoomInButton(myChart1)">+</button><button onclick="zoomOutButton(myChart1)">-</button>
-	<canvas id="myChart1"></canvas>
-			</div>
-		</div>
+	<!-- Graph -->
+	<div id="chartDiv" class="chartBox">
+	<label for="select-chart">Select Data:</label>
+	<select id="select-chart" onchange="updateGraph(myChart, 'select-chart')"></select>
+	<canvas id="myChart" style="width: 100%;"></canvas>
 	</div>
-
-
-	<!-- Second chart -->
-	<div class="container">
-		<div id="chart2Div" class="chartBox">
-	<label for="select-chart2">Select Data:</label>
-	<select id="select-chart2" onchange="updateGraph(myChart2, 'select-chart2')"></select><button onclick="resetZoomChart(myChart2)">Reset Zoom</button>
-	<canvas id="myChart2" style="height: 250px; width: 100%;"></canvas>
-		</div>
-	</div>
-
-	<!-- Third chart -->
-	<div class="container">
-		<div id="chart3Div" class="chartBox">
-	<label for="select-chart3">Select Data:</label>
-	<select id="select-chart3" onchange="updateGraph(myChart3, 'select-chart3')"></select><button onclick="resetZoomChart(myChart3)">Reset Zoom</button>
-	<canvas id="myChart3" style="height: 250px; width: 100%;"></canvas>
-		</div>
-	</div>
-
-	<!-- Fourth chart -->
-	<div class="container">
-		<div id="chart4Div" class="chartBox">
-	<label for="select-chart4">Select Data:</label>
-	<select id="select-chart4" onchange="updateGraph(myChart4, 'select-chart4')"></select><button onclick="resetZoomChart(myChart4)">Reset Zoom</button>
-	<canvas id="myChart4" style="height: 250px; width: 100%;"></canvas>
-		</div>
-	</div>
-
-	<!-- Fifth chart -->
-	<div class="container">
-		<div id="chart5Div" class="chartBox">
-	<label for="select-chart5">Select Data:</label>
-	<select id="select-chart5" onchange="updateGraph(myChart5, 'select-chart5')"></select><button onclick="resetZoomChart(myChart5)">Reset Zoom</button>
-	<canvas id="myChart5" style="height: 250px; width: 100%;"></canvas>
-		</div>
-	</div>
+	
 	
 	<!-- The script for updating the charts and checking json file. -->
 	<script src="app.js"></script>
