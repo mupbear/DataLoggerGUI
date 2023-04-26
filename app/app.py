094833b4ebb0f5b60757d27fea7f046b2850a39c
LeckerenSirupwaffeln <BM.Sevrin@gmail.com>
2023-04-26 14:43:43 +0000

    Added non-working before startup and before shutdown handlers for managing MySQL connection

--- app/app.py
+++ app/app.py
@@ -5,13 +5,24 @@ from litestar.contrib.jinja import JinjaTemplateEngine
 from litestar.response_containers import Template
 from litestar.template.config import TemplateConfig
 import logging
+import aiomysql
 
 from pathlib import Path
 
 logger = logging.getLogger("web-server-racing-data-analysis")
 
 async def before_startup_handler(app_instance: Litestar) -> None:
-    logger.debug("hello world")
+  logger.info("Connecting to MySQL database...")
+  app_instance.state.pool = await aiomysql.create_pool(
+    host='162.241.244.103', port=3306, 
+    user='regtersc_Joost', password='BroodjeKaas'
+  )
+
+async def before_shutdown_handler(app_instance: Litestar) -> None:
+  logger.info("Disconnecting from MySQL database...")
+  app_instance.pool.close()
+  await app_instance.pool.wait_closed()
+  logger.info("Disconnecting from MySQL database FINISHED")
 
 @get("/", media_type=MediaType.HTML, cache=False)
 async def get_root() -> Template:
@@ -33,23 +44,23 @@ def post_event(data: dict[str, str]) -> dict[str, str]:
   return data
 
 app = Litestar(
-    before_startup=[before_startup_handler],
-    route_handlers=[get_root, get_event, post_event],
-    template_config=TemplateConfig(
-        directory=Path("templates"),
-        engine=JinjaTemplateEngine,
-    ),
-    static_files_config=[
-        StaticFilesConfig(directories=[Path("static")], path="/static", name="static")
-    ],
-    logging_config=LoggingConfig(
-      loggers={
-          "web-server-racing-data-analysis": {
-              "propagate": False,
-              "formatters": {"standard": {"format": "%(levelname)s - %(asctime)s.%(msecs)06f - %(name)s - %(module)s - %(message)s", "datefmt": "%Y-%m-%d %H:%M:%S"}},
-              "level": "DEBUG",
-              "handlers": ["queue_listener"],
-          }
+  before_startup=[before_startup_handler],
+  before_shutdown=[before_shutdown_handler],
+  route_handlers=[get_root, get_event, post_event],
+  template_config=TemplateConfig(
+    directory=Path("templates"),
+    engine=JinjaTemplateEngine,
+  ),
+  static_files_config=[
+    StaticFilesConfig(directories=[Path("static")], path="/static", name="static")
+  ],
+  logging_config=LoggingConfig(
+    loggers={
+      "web-server-racing-data-analysis": {
+          "propagate": False,
+          "level": "INFO",
+          "handlers": ["queue_listener"],
       }
-    )
+    }
+  )
 )
